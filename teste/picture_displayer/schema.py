1b0424e7a91cf9cd75a71ca8b2992998615377e8
Pedro Abilio <pedro_abilio23@hotmail.com>
2020-06-16 17:53:25 +0100

    Createed all the mutations and querys for the api
    All of the requested mutations and querys where created and all the actions are being made on the database

--- teste/picture_displayer/schema.py
+++ teste/picture_displayer/schema.py
@@ -5,9 +5,58 @@ from .models import Image
 class ImageType(DjangoObjectType):
     class Meta:
         model = Image
+        fields = '__all__'
 
 class Query(graphene.ObjectType):
     all_images = graphene.List(ImageType)
+    image = graphene.Field(ImageType, image_id=graphene.ID())
 
     def resolve_all_images(self, info, **kwargs):
         return Image.objects.all()
+    def resolve_image(self, info, image_id):
+        return Image.objects.get(pk=image_id)
+   
+
+class CreateImageMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        image = graphene.String()
+
+    added_image = graphene.Field(ImageType, name=graphene.String(),image = graphene.String())
+    ok = graphene.Boolean()
+    def mutate(root, info, name, image):
+        image = Image(name=name, image=image)
+        image.save()
+        ok = True
+        return CreateImageMutation(added_image=image, ok=ok)
+
+class UpdateImageMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String()
+        image = graphene.String()
+
+    added_image = graphene.Field(ImageType, name=graphene.String(),image = graphene.String())
+    ok = graphene.Boolean()
+
+    def mutate(root, info, id, name, image):
+        Image.objects.filter(pk=id).update(name= name, image=image)
+        ok = True
+        return CreateImageMutation(added_image=Image.objects.get(pk=id), ok=ok)
+
+class DeleteImageMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    added_image = graphene.Field(ImageType, name=graphene.String(),image = graphene.String())
+    ok = graphene.Boolean()
+
+    def mutate(root, info, id):
+        Image.objects.filter(id=id).delete()
+        ok = True
+        return CreateImageMutation(ok=ok)
+
+class Mutations(graphene.ObjectType):
+    create_image_mutation = CreateImageMutation.Field()
+    update_image_mutaion = UpdateImageMutation.Field()
+    delete_image_mutation = DeleteImageMutation.Field()
--- teste/teste/schema.py
+++ teste/teste/schema.py
@@ -1,7 +1,11 @@
 import graphene
 from picture_displayer.schema import Query as image_query
+from picture_displayer.schema import Mutations as image_mutation
 
 class Query(image_query):
     pass
 
-schema = graphene.Schema(query=Query)
+class Mutations(image_mutation):
+    pass
+
+schema = graphene.Schema(query=Query,mutation=Mutations)
